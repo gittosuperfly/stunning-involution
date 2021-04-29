186305a2f9934d96ea44537798e574da79af5ac3
爱吃菜的汪 <superfly1106@gmail.com>
2021-04-29 17:12:14 +0800

    code

---
+++ src/leetcode/LC003.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class LC003 {
+    public int lengthOfLongestSubstring(String s) {
+        //空处理
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int max = 0;
+        int pre = 0;
+        int next = 0;
+        //存放无重复子串的所有元素
+        HashSet<Character> set = new HashSet<>();
+        while (pre < s.length() && next < s.length()) {
+            //无重复则下一个，有重复则去尾再判断
+            if (!set.contains(s.charAt(pre))) {
+                set.add(s.charAt(pre));
+                pre++;
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(next));
+                next++;
+            }
+        }
+        return max;
+    }
+}
